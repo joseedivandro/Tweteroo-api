337e3279da1c2b9f717b0e596d7aee8df8a91f23
joseedivandro <jose.edivandro.ja@outlook.com>
2023-04-05 17:43:07 -0300

    instalando cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import  express from "express";
+import cors from "cors";
 
 const PORT =5000;
 const app = express() // app do servidor 
 
 
+app.use(cors())
 app.use(express.json)()
 app.listen(PORT, () => console.log(`rodando servidor na porta ${PORT}`))
 
 app.post("./sign-up", (req, res)=> {
 
     const user = req.body
-    
+
 })

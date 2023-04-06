af48ce9d1a97a93095adf9aaa04a7c90b5ae43a9
joseedivandro <jose.edivandro.ja@outlook.com>
2023-04-06 18:43:49 -0300

    tweets

--- src/app.js
+++ src/app.js
@@ -26,10 +26,21 @@ app.post("/sign-up", (req, res)=> {
     usuarios.push(user)
     res.status(200).send("OK")
     console.log("deu certo")
-}
+})
 
 
+app.post("/tweets", (req, res) => {
+    const twetter = req.body
+    if (!twetter.tweet) return res.status(400).send("Todos os campos são obrigatórios!")
+    if (!(typeof twetter.tweet === "string")) return res.sendStatus(400)
 
-)
+    const userRegistered = serverUsers.find(item => item.username === twetter.username)
+
+    if (userRegistered) {
+        serverTweets.push(twetter)
+        return res.status(201).send("OK")
+    }
+    return res.status(401).send("UNAUTHORIZED")
+})
 
 app.listen(PORT, () => console.log(`rodando servidor na porta ${PORT}`))

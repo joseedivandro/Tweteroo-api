62a2fdec15bac6c3745c6e35668d3908a3f89874
joseedivandro <jose.edivandro.ja@outlook.com>
2023-04-06 09:39:43 -0300

    corrigindo primeiro post

--- src/app.js
+++ src/app.js
@@ -8,24 +8,28 @@ const usuarios = []
 
 app.use(cors())
 app.use(express.json())
-app.listen(PORT, () => console.log(`rodando servidor na porta ${PORT}`))
 
-app.post("./sign-up", (req, res)=> {
+
+app.post("/sign-up", (req, res)=> {
 
     const user = req.body
     if(!user.username || !user.avatar){
-        return res.status(400).send("Todos os Campos obrigatórios!")
+        return res.status(400).send("Todos os Campos são obrigatórios!")
     }
 
     const TipoElemento = (typeof user.username === "string" && typeof user.avatar === "string")
 
     if(!TipoElemento){
         return res.sendStatus(400)
+        console.log("deu errado")
     }
     usuarios.push(user)
     res.status(200).send("OK")
+    console.log("deu certo")
 }
 
 
 
 )
+
+app.listen(PORT, () => console.log(`rodando servidor na porta ${PORT}`))

2163bac4013f86fe34de9eacc2b0b914ef0b1e51
joseedivandro <jose.edivandro.ja@outlook.com>
2023-04-06 08:51:45 -0300

    cadastrando usuarios

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 const PORT =5000;
 const app = express() // app do servidor 
 
+const usuarios = []
 
 app.use(cors())
 app.use(express.json())
@@ -12,5 +13,19 @@ app.listen(PORT, () => console.log(`rodando servidor na porta ${PORT}`))
 app.post("./sign-up", (req, res)=> {
 
     const user = req.body
+    if(!user.username || !user.avatar){
+        return res.status(400).send("Todos os Campos obrigatórios!")
+    }
 
-})
+    const TipoElemento = (typeof user.username === "string" && typeof user.avatar === "string")
+
+    if(!TipoElemento){
+        return res.sendStatus(400)
+    }
+    usuarios.push(user)
+    res.status(200).send("OK")
+}
+
+
+
+)
